ce39600aa7e1668fc74947ae085e4d98a63397d3
alyssajin <alyssajin2019@gmail.com>
2024-07-19 16:37:50 -0700

    feat: created the structure of navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import logo from '../../assets/logo/logo.png'
+import React from "react"
+import logo from "../../assets/logo/logo.png"
+import cart from "../../assets/icons/cart.png"
 
 export const Navbar = () => {
 
   return (
     <div className='navbar'>
+        <div className="nav-login">
+            <button className="btn-primary">Login</button>
+        </div>
         <div className="nav-logo">
-            <img src={logo} alt="" />
-            <p>E Commerce</p>
+            <div className="nav-logo-icon">
+                <img src={logo} alt="" />
+            </div>
+            <div className="nav-search">
+                <input type="text" placeholder="Search for products" />
+                <button className="nav-search-btn">Search</button>
+            </div>
+            <div className="nav-cart">
+                <img src={cart} alt="" />
+            <div className="nav-cart-count">0</div>
+        </div>
         </div>
         <ul className="nav-menu">
             <li>Home</li>
@@ -15,6 +28,7 @@ export const Navbar = () => {
             <li>MAN</li>
             <li>KIDS</li>
         </ul>
+       
     </div>
   )
 }

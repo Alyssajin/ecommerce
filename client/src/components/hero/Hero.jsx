a4b6febf161ffe91d0ad46b55bd7687fe82eae84
alyssajin <alyssajin2019@gmail.com>
2024-07-21 14:13:33 -0700

    feat: created the hero part

---
+++ client/src/components/hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-img-container">
+            <img src="//images.ctfassets.net/q602vtcuu3w3/7wkCiB2MLoMDqX7XWLuMVE/7715ffe7a90fb8b130ba9d18e78656dd/24-PROMO-CORE-CLEARNACE-SALE-751912.jpg?w=2880&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
+                alt=""
+                fetchpriority="auto">
+            </img>
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,23 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import Hero from "../components/hero/Hero";
 
 export default function Home() {
   const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
 
   return (
-    <div className="home">
-      {/* <h1>Assignment 3</h1>
-      <div>
-        <button className="btn-primary" onClick={loginWithRedirect}>
-          Login
-        </button>
-        <p>Loading: {String(isLoading)} </p>
-        <p>User Authenticated: {String(isAuthenticated)}</p>
-      </div> */}
+    <div>
+      <Hero />
     </div>
+
+    // <div className="home">
+    //   <h1>Assignment 3</h1>
+    //   <div>
+    //     <button className="btn-primary" onClick={loginWithRedirect}>
+    //       Login
+    //     </button>
+    //     <p>Loading: {String(isLoading)} </p>
+    //     <p>User Authenticated: {String(isAuthenticated)}</p>
+    //   </div>
+    // </div>
   );
 }

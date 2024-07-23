153677201330a1459b580d9497e9e1059ffc7133
alyssajin <alyssajin2019@gmail.com>
2024-07-22 17:37:04 -0700

    feat: added links for the popular part

--- client/src/components/popular/Popular.jsx
+++ client/src/components/popular/Popular.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './Popular.css'
+import { Link } from 'react-router-dom'
 
 const Popular = () => {
   return (
     <div className='popular'>
         <div className="popular-container">
             <div className="popular-content-container">
+                
                 <div className="popular-inner-top">
-                    <img src="//images.ctfassets.net/q602vtcuu3w3/7FN9jV5pR4KceNjErZ4DKw/c33bcf73d1bafe5b373ee0487389a916/24_JULY_HOMEPAGE_ASSETS_EM64__1_.jpg?w=1420&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
-                        alt=" " 
-                        fetchpriority="auto">
-                    </img>
+                    <Link to="/women">
+                        <img src="//images.ctfassets.net/q602vtcuu3w3/7FN9jV5pR4KceNjErZ4DKw/c33bcf73d1bafe5b373ee0487389a916/24_JULY_HOMEPAGE_ASSETS_EM64__1_.jpg?w=1420&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
+                            alt=" " 
+                            fetchpriority="auto">
+                        </img>
+                    </Link>
                 </div>
                 <div className="popular-inner-middle">
                     <p className="inner-text-title">
@@ -31,10 +35,12 @@ const Popular = () => {
 
             <div className="popular-content-container">
                 <div className="popular-inner-top">
-                    <img src="//images.ctfassets.net/q602vtcuu3w3/2phu60i1EqhFkzbnyuboAr/027f792503dbff07722cafbfeaa61c7a/24_JULY_HOMEPAGE_ASSETS_EM12.jpg?w=1420&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
-                        alt=" " 
-                        fetchpriority="auto">
-                    </img>
+                    <Link to="/men">
+                        <img src="//images.ctfassets.net/q602vtcuu3w3/2phu60i1EqhFkzbnyuboAr/027f792503dbff07722cafbfeaa61c7a/24_JULY_HOMEPAGE_ASSETS_EM12.jpg?w=1420&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
+                            alt=" " 
+                            fetchpriority="auto">
+                        </img>
+                    </Link>
                 </div>
                 <div className="popular-inner-middle">
                     <p className="inner-text-title">
@@ -54,10 +60,12 @@ const Popular = () => {
             </div>
             <div className="popular-content-container">
                 <div className="popular-inner-top">
-                    <img src="//images.ctfassets.net/q602vtcuu3w3/3z0wxYTVPgyzgxgYpMfK6B/50db539dac40c49f5d562c5a8b40cfdb/24_JULY_HOMEPAGE_ASSETS_EM2__1_.jpg?w=710&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
-                        alt=" " 
-                        fetchpriority="auto">
-                    </img>
+                    <Link to="/kid">
+                        <img src="//images.ctfassets.net/q602vtcuu3w3/3z0wxYTVPgyzgxgYpMfK6B/50db539dac40c49f5d562c5a8b40cfdb/24_JULY_HOMEPAGE_ASSETS_EM2__1_.jpg?w=710&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
+                            alt=" " 
+                            fetchpriority="auto">
+                        </img>
+                    </Link>
                 </div>
                 <div className="popular-inner-middle">
                     <p className="inner-text-title">
@@ -77,10 +85,12 @@ const Popular = () => {
             </div>
             <div className="popular-content-container">
                 <div className="popular-inner-top">
-                    <img src="//images.ctfassets.net/q602vtcuu3w3/4kKJY3JU476F1SvHpjRfyv/30a58cc854fbbf4b2e7f61697667fed3/24_JULY_HOMEPAGE_ASSETS_EM59.jpg?w=1420&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
-                        alt=" " 
-                        fetchpriority="auto">
-                    </img>
+                    <Link to="/sale">
+                        <img src="//images.ctfassets.net/q602vtcuu3w3/4kKJY3JU476F1SvHpjRfyv/30a58cc854fbbf4b2e7f61697667fed3/24_JULY_HOMEPAGE_ASSETS_EM59.jpg?w=1420&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
+                            alt=" " 
+                            fetchpriority="auto">
+                        </img>
+                    </Link>
                 </div>
                 <div className="popular-inner-middle">
                     <p className="inner-text-title">

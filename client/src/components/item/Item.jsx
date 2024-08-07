d586167b22762e5346570a29d7466623b35b7f25
Yuning <mu.yun@northeastern.edu>
2024-08-07 10:31:48 -0700

    feat: added cart button and moved item related css to components/item folder

--- client/src/components/item/Item.jsx
+++ client/src/components/item/Item.jsx
@@ -6,11 +6,20 @@ const Item = (props) => {
 
   return (
     <div className='item'>
-        <Link to={`/shop/${props.id}`}><img onClick={window.scroll(0,0)} src={props.image} alt=""/> </Link>
+        <Link to={`/shop/${props.id}`}>
+          <div className='item-image-container'>
+            <img onClick={window.scroll(0,0)} src={props.image} alt=""/>
+            <div className='item-add-container'>
+              <button className='item-add-button'>Add to Cart</button>
+            </div>
+          </div>
+        </Link>
+
         <div className='item-brand'>
             <p>{props.brand}</p>
         </div>
         <div className='item-name'>
+            {/*  should names be prefixed with '$'? */}
             <p>${props.name}</p>
         </div>
         <div className='item-price'>

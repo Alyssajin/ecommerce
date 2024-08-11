2a70b8acc28b9f6ee5b7660adadef74fde57a0b0
alyssajin <alyssajin2019@gmail.com>
2024-08-11 15:51:54 -0700

    fix: redirect the anonymous user to the login page while clicking the add to  cart btm

--- client/src/components/item/Item.jsx
+++ client/src/components/item/Item.jsx
@@ -3,18 +3,26 @@ import './Item.css'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../cartItem/CartContext'
 import { useContext } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
 
 const Item = (props) => {
   const {addToCart} = useContext(CartContext)
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const logIn = () => loginWithRedirect();
 
-
+ 
   return (
     <div className='item'>
         <Link to={`/shop/${props.id}`}>
           <div className='item-image-container'>
             <img onClick={window.scroll(0,0)} src={props.image} alt=""/>
             <div className='item-add-container'>
-              <button className='item-add-button'onClick={()=>addToCart(props.id)} >Add to Cart</button>
+              { !isAuthenticated ? (
+                <button className='item-add-button' onClick={logIn}>Add to Cart</button>
+              ) : (
+                <button className='item-add-button'onClick={()=>addToCart(props.id)}  >Add to Cart</button>
+              )}
             </div>
           </div>
         </Link>

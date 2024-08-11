9bb13462b44504e8bcd18ef0716dd64acb06d079
alyssajin <alyssajin2019@gmail.com>
2024-08-11 15:09:18 -0700

    feat: products in the web page will automatically be fetched from the database

---
+++ client/src/context/ShopContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useEffect, useState } from 'react'
+export const ShopContext = createContext()
+
+
+const ShopContextProvider = (props) => {
+    const [all_products, setAllProducts] = useState([])
+    useEffect(() => {
+        // fetch data from server
+        fetch('http://localhost:8000/products', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response => response.json())
+        .then((data) => {
+            setAllProducts(data.products);
+        })
+        .catch((error) => {
+            console.error("Error fetching products:", error);
+        });
+    }, []);
+
+
+    const contextValue = {all_products}
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider

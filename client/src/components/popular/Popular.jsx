b11dec89f8cf7691d89d82ae3772e9b105051bee
alyssajin <alyssajin2019@gmail.com>
2024-08-12 17:08:39 -0700

    doc: added alt value for each img

--- client/src/components/popular/Popular.jsx
+++ client/src/components/popular/Popular.jsx
@@ -11,7 +11,7 @@ const Popular = () => {
                 <div className="popular-inner-top">
                     <Link to="/women">
                         <img src="//images.ctfassets.net/q602vtcuu3w3/7FN9jV5pR4KceNjErZ4DKw/c33bcf73d1bafe5b373ee0487389a916/24_JULY_HOMEPAGE_ASSETS_EM64__1_.jpg?w=1420&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
-                            alt=" " 
+                            alt="women" 
                             fetchpriority="auto">
                         </img>
                     </Link>
@@ -37,7 +37,7 @@ const Popular = () => {
                 <div className="popular-inner-top">
                     <Link to="/men">
                         <img src="//images.ctfassets.net/q602vtcuu3w3/2phu60i1EqhFkzbnyuboAr/027f792503dbff07722cafbfeaa61c7a/24_JULY_HOMEPAGE_ASSETS_EM12.jpg?w=1420&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
-                            alt=" " 
+                            alt="men" 
                             fetchpriority="auto">
                         </img>
                     </Link>
@@ -62,7 +62,7 @@ const Popular = () => {
                 <div className="popular-inner-top">
                     <Link to="/kids">
                         <img src="//images.ctfassets.net/q602vtcuu3w3/3z0wxYTVPgyzgxgYpMfK6B/50db539dac40c49f5d562c5a8b40cfdb/24_JULY_HOMEPAGE_ASSETS_EM2__1_.jpg?w=710&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
-                            alt=" " 
+                            alt="kids" 
                             fetchpriority="auto">
                         </img>
                     </Link>
@@ -87,7 +87,7 @@ const Popular = () => {
                 <div className="popular-inner-top">
                     <Link to="/sale">
                         <img src="//images.ctfassets.net/q602vtcuu3w3/4kKJY3JU476F1SvHpjRfyv/30a58cc854fbbf4b2e7f61697667fed3/24_JULY_HOMEPAGE_ASSETS_EM59.jpg?w=1420&amp;q=80&amp;fm=jpg&amp;fl=progressive" 
-                            alt=" " 
+                            alt="sale" 
                             fetchpriority="auto">
                         </img>
                     </Link>
